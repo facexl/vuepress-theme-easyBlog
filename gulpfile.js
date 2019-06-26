dab2a2805555230cc8563ca874834a93477024d7
xiaolang <facelanglang@gmail.com>
2019-06-26 23:27:03 +0800

    del

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-const gulp  = require('gulp')
-const watch = require('gulp-watch')
-const createCategory = require('./createCategory')
-gulp.task('watch',function(){
-    return watch('blog/**', function (e) {
-        createCategory()
-    });
-})
