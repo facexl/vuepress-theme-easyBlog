77ce9150c7f830333a73c118236f1ce1c7540dd2
xiaolang <facelanglang@gmail.com>
2019-06-28 09:39:36 +0800

    不怂

--- .vuepress/_private.js
+++
@@ -1,4 +0,0 @@
-export default {
-    gitalk_id:'',
-    gitalk_sec:''
-}
--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,13 +2,13 @@ module.exports = {
     title: '小浪博客',
     description: '肖浪的博客网站,由vuepress驱动,主要记录前端技术',
     head: [
-        ['link', { rel: 'icon', href: 'https://avatars3.githubusercontent.com/u/25611177?s=460&v=4' }],
+        ['link', { rel: 'icon', href: 'http://img.xlcool.cn/Fnlnja7TctRza5Q-IcPPN8liAxLY' }],
         ['link', { rel:"stylesheet",href: 'https://cdn.bootcss.com/gitalk/1.5.0/gitalk.min.css' }],
         ['script', { src: 'https://cdn.bootcss.com/gitalk/1.5.0/gitalk.min.js' }]
     ],
     themeConfig: {
         githubLink:'https://github.com/facexl/facexl.github.io',
-        avatar:'https://avatars3.githubusercontent.com/u/25611177?s=460&v=4',
+        avatar:'http://img.xlcool.cn/Fnlnja7TctRza5Q-IcPPN8liAxLY',
         nav: [
           { text: 'Home', link: '/' },
           { text: 'All', link: '/category/all/1' },
